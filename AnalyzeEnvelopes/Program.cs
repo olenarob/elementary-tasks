eba9dc607d71495564ed5353f6d71d5481ae3317
olenarob <olenarobotko@gmail.com>
2021-04-26 23:25:46 +0300

    Add validation

--- AnalyzeEnvelopes/Program.cs
+++ AnalyzeEnvelopes/Program.cs
@@ -6,44 +6,87 @@ namespace AnalyzeEnvelopes
     {
         static void Main(string[] args)
         {
+            
             double[] parameters = new double[4];
             int i = 0;
 
-            foreach (string s in args)
+            /*
+            if (args.Length >= 4)
             {
-                parameters[i] = double.Parse(s);
-                i++;
-            }
-
-            if (i == 0)
-            {
-                string s;
                 for (i = 0; i < 4; i++)
                 {
-                    Console.Write($"Parameter{i}: ");
-                    parameters[i] = double.Parse(Console.ReadLine());
+                    parameters[i] = double.Parse(args[i]);
+                    Console.WriteLine($"Parameter{i}: {parameters[i]}");
                 }
             }
-           
-            Envelope envelope1 = new Envelope(parameters[0], parameters[1]);
-            Envelope envelope2 = new Envelope(parameters[2], parameters[3]);
-
-            int smallEnvelope = 0;
-            int bigEnvelope = 0;
-
-            if (envelope1.CanBeInsertedInto(envelope2))
+            else
             {
-                smallEnvelope = 1;
-                bigEnvelope = 2;
-            }
-            else if (envelope2.CanBeInsertedInto(envelope1))
-            {
-                smallEnvelope = 2;
-                bigEnvelope = 1;
-            }
+            */
+                string desireToContinue;
+                do
+                {
+                    for (i = 0; i < 4; i++)
+                    {
+                        try
+                        {
+                            if (args.Length >= 4)
+                            {
+                                parameters[i] = double.Parse(args[i]);
+                                Console.WriteLine($"Parameter{i}: {parameters[i]}");
+                            }
+                            else
+                            {
+                                Console.Write($"Please input parameter{i}: ");
+                                parameters[i] = double.Parse(Console.ReadLine());
+                            }
+                            if (parameters[i] <= 0)
+                            {
+                                throw new ArgumentOutOfRangeException("Size of envelope must be greater than zero!");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        if (args.Length < 4)
+                        {
+                            i--;
+                        }
+                        else break;
+                        }
+                    }
 
-            Console.WriteLine($"Envelope {smallEnvelope} can be inserted into envelope {bigEnvelope}");
-            
+                    
+
+                    Envelope envelope1 = new Envelope(parameters[0], parameters[1]);
+                    Envelope envelope2 = new Envelope(parameters[2], parameters[3]);
+
+                    int smallEnvelope = 0;
+                    int bigEnvelope = 0;
+
+                    if (envelope1.CanBeInsertedInto(envelope2))
+                    {
+                        smallEnvelope = 1;
+                        bigEnvelope = 2;
+                    }
+                    else if (envelope2.CanBeInsertedInto(envelope1))
+                    {
+                        smallEnvelope = 2;
+                        bigEnvelope = 1;
+                    }
+
+                    if (smallEnvelope == 0)
+                    {
+                        Console.WriteLine("None of the envelopes cannot be inserted into the other!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Envelope {smallEnvelope} can be inserted into envelope {bigEnvelope}");
+                    }
+                    
+                    Console.Write("Do you want to continue? ");
+                    desireToContinue = Console.ReadLine();
+                }
+                while (desireToContinue == "y");
             
         }
     }
