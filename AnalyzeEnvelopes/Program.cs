d2491ad811d843a04467387f94403b2666c394ad
olenarob <olenarobotko@gmail.com>
2021-04-26 17:03:46 +0300

    Add SLN file

--- AnalyzeEnvelopes/Program.cs
+++ AnalyzeEnvelopes/Program.cs
@@ -1,19 +1,55 @@
 ﻿using System;
 
-namespace AnalizeEnvelopes
+namespace AnalyzeEnvelopes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Envelope envelope1 = new Envelope(3.64592, 4.1588902627878);
-            Envelope envelope2 = new Envelope(4.15889026278, 3.64592);
+            double[] parameters = new double[4];
+            int i = 0;
 
-            Console.WriteLine(envelope1.CanBeInsertedIn(envelope2));
-            Console.WriteLine(envelope2.CanBeInsertedIn(envelope1));
+            foreach (string s in args)
+            {
+                parameters[i] = double.Parse(s);
+                i++;
+            }
+
+            if (i == 0)
+            {
+                string s;
+                for (i = 0; i < 4; i++)
+                {
+                    Console.Write($"Parameter{i}: ");
+                    parameters[i] = double.Parse(Console.ReadLine());
+                }
+            }
+           
+            Envelope envelope1 = new Envelope(parameters[0], parameters[1]);
+            Envelope envelope2 = new Envelope(parameters[2], parameters[3]);
+
+            int smallEnvelope = 0;
+            int bigEnvelope = 0;
+
+            if (envelope1.CanBeInsertedInto(envelope2))
+            {
+                smallEnvelope = 1;
+                bigEnvelope = 2;
+            }
+            else if (envelope2.CanBeInsertedInto(envelope1))
+            {
+                smallEnvelope = 2;
+                bigEnvelope = 1;
+            }
+
+            Console.WriteLine($"Envelope {smallEnvelope} can be inserted into envelope {bigEnvelope}");
+            
+            
         }
     }
 
+    
+    
     public class Envelope
     {
         private double _shortSide;
@@ -33,7 +69,7 @@ namespace AnalizeEnvelopes
             }
         }
 
-        public bool CanBeInsertedIn (Envelope that)
+        public bool CanBeInsertedInto (Envelope that)
         {
             if ((this._shortSide < that._shortSide) && (this._longSide < that._longSide))
             {
