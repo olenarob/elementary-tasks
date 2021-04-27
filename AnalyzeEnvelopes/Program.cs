8e20a4a63be55d78d474e86d843f1a5fe5e94ba7
olenarob <olenarobotko@gmail.com>
2021-04-27 08:49:10 +0300

    Fixed bugs

--- AnalyzeEnvelopes/Program.cs
+++ AnalyzeEnvelopes/Program.cs
@@ -6,88 +6,74 @@ namespace AnalyzeEnvelopes
     {
         static void Main(string[] args)
         {
-            
             double[] parameters = new double[4];
             int i = 0;
 
-            /*
-            if (args.Length >= 4)
+            string desireToContinue;
+            do
             {
                 for (i = 0; i < 4; i++)
                 {
-                    parameters[i] = double.Parse(args[i]);
-                    Console.WriteLine($"Parameter{i}: {parameters[i]}");
-                }
-            }
-            else
-            {
-            */
-                string desireToContinue;
-                do
-                {
-                    for (i = 0; i < 4; i++)
+                    try
                     {
-                        try
+                        if (args.Length >= 4)
                         {
-                            if (args.Length >= 4)
-                            {
-                                parameters[i] = double.Parse(args[i]);
-                                Console.WriteLine($"Parameter{i}: {parameters[i]}");
-                            }
-                            else
-                            {
-                                Console.Write($"Please input parameter{i}: ");
-                                parameters[i] = double.Parse(Console.ReadLine());
-                            }
-                            if (parameters[i] <= 0)
-                            {
-                                throw new ArgumentOutOfRangeException("Size of envelope must be greater than zero!");
-                            }
+                            parameters[i] = double.Parse(args[i]);
+                            Console.WriteLine($"Parameter{i}: {parameters[i]}");
                         }
-                        catch (Exception ex)
+                        else
                         {
-                            Console.WriteLine(ex.Message);
+                            Console.Write($"Please input parameter{i}: ");
+                            parameters[i] = double.Parse(Console.ReadLine());
+                        }
+                        if (parameters[i] <= 0)
+                        {
+                            throw new ArgumentOutOfRangeException("Size of envelope must be greater than zero!");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
                         if (args.Length < 4)
                         {
                             i--;
                         }
                         else break;
-                        }
                     }
-
-                    
-
-                    Envelope envelope1 = new Envelope(parameters[0], parameters[1]);
-                    Envelope envelope2 = new Envelope(parameters[2], parameters[3]);
-
-                    int smallEnvelope = 0;
-                    int bigEnvelope = 0;
-
-                    if (envelope1.CanBeInsertedInto(envelope2))
-                    {
-                        smallEnvelope = 1;
-                        bigEnvelope = 2;
-                    }
-                    else if (envelope2.CanBeInsertedInto(envelope1))
-                    {
-                        smallEnvelope = 2;
-                        bigEnvelope = 1;
-                    }
-
-                    if (smallEnvelope == 0)
-                    {
-                        Console.WriteLine("None of the envelopes cannot be inserted into the other!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Envelope {smallEnvelope} can be inserted into envelope {bigEnvelope}");
-                    }
-                    
-                    Console.Write("Do you want to continue? ");
-                    desireToContinue = Console.ReadLine();
                 }
-                while (desireToContinue == "y");
-            
+
+                args = new string[1];
+
+                Envelope envelope1 = new Envelope(parameters[0], parameters[1]);
+                Envelope envelope2 = new Envelope(parameters[2], parameters[3]);
+
+                int smallEnvelope = 0;
+                int bigEnvelope = 0;
+
+                if (envelope1.CanBeInsertedInto(envelope2))
+                {
+                    smallEnvelope = 1;
+                    bigEnvelope = 2;
+                }
+                else if (envelope2.CanBeInsertedInto(envelope1))
+                {
+                    smallEnvelope = 2;
+                    bigEnvelope = 1;
+                }
+
+                if (smallEnvelope == 0)
+                {
+                    Console.WriteLine("None of the envelopes cannot be inserted into the other!");
+                }
+                else
+                {
+                    Console.WriteLine($"Envelope {smallEnvelope} can be inserted into envelope {bigEnvelope}");
+                }
+
+                Console.Write("Do you want to continue? ");
+                desireToContinue = Console.ReadLine();
+            }
+            while (desireToContinue == "y");
         }
     }
 
