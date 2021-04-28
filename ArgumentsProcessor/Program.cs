d13548849e0f1fb55ac274a07cdd2a45210b94ce
olenarob <olenarobotko@gmail.com>
2021-04-28 11:18:38 +0300

    Added ArgumentsProcessor

---
+++ ArgumentsProcessor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArgumentsProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Argument
+    {
+        
+        public static bool TryParseInt(string s, int min, int max, out int value)
+        {
+            bool success = true;
+            try
+            {
+                    value = int.Parse(s);
+
+                    if ((value < min) || (value > max))
+                        throw new ArgumentOutOfRangeException();
+            }
+            catch
+            {
+                success = false;
+                value = 0;
+            }
+            return success;
+        }
+
+    }
+}
