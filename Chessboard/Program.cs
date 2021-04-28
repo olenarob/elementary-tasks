4830dcad82d11e4682793eafe07743fd0b8266a3
olenarob <olenarobotko@gmail.com>
2021-04-28 11:00:43 +0300

    Added changes

--- Chessboard/Program.cs
+++ Chessboard/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArgumentsProcessor;
+using System;
 
 namespace Chessboard
 {
@@ -6,34 +7,23 @@ namespace Chessboard
     {
         static void Main(string[] args)
         {
-            int[] maxSizeOfChessboard = new int[2];
-            maxSizeOfChessboard[0] = Console.LargestWindowWidth;
-            maxSizeOfChessboard[1] = Console.LargestWindowHeight;
+            int width = 0;
+            int height = 0;
 
-            int[] sizeOfChessboard = new int[2];
-
-            if (args.Length == 2)
+ 
+            if ((args.Length == 2) &&
+                Argument.TryParseInt(args[0], 1, Console.LargestWindowWidth, out width) &&
+                Argument.TryParseInt(args[1], 1, Console.LargestWindowHeight, out height))
             {
-                try
-                {
-                    for (int i = 0; i < 2; i++)
-                    {
-                        sizeOfChessboard[i] = int.Parse(args[i]);
-
-                        if (sizeOfChessboard[i] > maxSizeOfChessboard[i])
-                            throw new ArgumentOutOfRangeException();
-                    }
-
-                    Chessboard chessboard = new Chessboard(sizeOfChessboard[0], sizeOfChessboard[1]);
-                    chessboard.PrintChessboard();
-                }
-                catch (Exception ex)
-                {
-                    //Console.WriteLine(ex);
-                    Console.WriteLine("Usage: Chessboard.exe 8 8");
-                }
-
+                Console.WriteLine(args.Length);
+                Chessboard chessboard = new Chessboard(width, height);
+                chessboard.PrintChessboard();
             }
+            else
+            {
+                Console.WriteLine("Usage: Chessboard.exe [width] [height]");
+            }
+            
             //Console.ReadKey();
 
         }
@@ -43,14 +33,16 @@ namespace Chessboard
             private int numberOfRows;
             private int numberOfColumns;
             
-            public Chessboard(int numberOfRows, int numberOfColumns)
+            public Chessboard(int width, int height)
             {
-                this.numberOfRows = numberOfRows;
-                this.numberOfColumns = numberOfColumns;
+                numberOfColumns = width;
+                numberOfRows = height;
             }
 
             public void PrintChessboard()
             {
+                Console.SetBufferSize(numberOfColumns+1,numberOfRows);
+
                 for (int i = 0; i < numberOfRows; i++)
                 {
                     if ((i % 2) == 1)
