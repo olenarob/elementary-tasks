ac91f8c32b671e69c9a8e9630251337521af285e
olenarob <olenarobotko@gmail.com>
2021-05-05 18:10:33 +0300

    Sequence works

--- NumericalSequence/Program.cs
+++ NumericalSequence/Program.cs
@@ -7,22 +7,74 @@ namespace NumericalSequence
         static void Main(string[] args)
         {
             int n = int.Parse(args[0]);
-            int m = (int)Math.Sqrt(int.Parse(args[1]));
-            //int[] a = new int[n];
-            if a*a
-            int a = m;
-            for (int i = 0; i < n; i++)
-            {
-                a++;
-                //= a[i-1]+1;
-                Console.Write($"{a} ");
-            }
+            int m = int.Parse(args[1]);
+
+            int lowerRange = (int)Math.Ceiling(Math.Sqrt(m));
+            
+            Sequence naturalNumbers = new NaturalNumbers();
+            naturalNumbers.DisplaySequence(lowerRange, length: n);
+            naturalNumbers.DisplaySequence();
+
+            Sequence fibonacci = new Fibonaccy();
+            fibonacci.DisplaySequence(length: n);
+            fibonacci.DisplaySequence(13, 1000);
         }
 
-        public class NumericalSeries
+        public abstract class Sequence
         {
-            private int n;
-            private i
+            public abstract int CommonElement(int n);
+
+            public void DisplaySequenceOfLength (int index, int length)
+            {
+                for (int i = index; i < index + length; i++)
+                {
+                    Console.Write($"{CommonElement(i)} ");
+                }
+                Console.WriteLine();
+            }
+            public void DisplaySequence(int lowerRange = 0, int upperRange = int.MaxValue, int length = 0)
+            {
+                int index = 0;
+                int i = 0;
+                bool flag = true;
+                while (((length == 0) && (CommonElement(i) <= upperRange)) || (i < index + length))
+                {
+                    if (CommonElement(i) >= lowerRange)
+                    {
+                        if (flag)
+                        {
+                            index = i;
+                            flag = false;
+                        }
+                        Console.Write($"{CommonElement(i)} ");
+                    }
+                    i++;
+                }
+                Console.WriteLine();
+            }
+        }
+        public class NaturalNumbers : Sequence
+        {
+            public override int CommonElement (int n)
+            {
+                return n switch
+                {
+                    0 => 0,
+                    _ => (CommonElement(n - 1) + 1),
+                };
+            }
+        }
+        public class Fibonaccy : Sequence
+        {
+            public override int CommonElement(int n)
+            {
+                return n switch
+                {
+                    0 => 0,
+                    1 => 1,
+                    _ => (CommonElement(n - 1) + CommonElement(n - 2)),
+                };
+            }
         }
     }
 }
