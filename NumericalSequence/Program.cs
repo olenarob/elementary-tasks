eeb48ce239f6defebc202ca3bd32c6ac3f0b85ed
olenarob <olenarobotko@gmail.com>
2021-05-05 14:05:13 +0300

    Created NumericalSequence

---
+++ NumericalSequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumericalSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int m = (int)Math.Sqrt(int.Parse(args[1]));
+            //int[] a = new int[n];
+            if a*a
+            int a = m;
+            for (int i = 0; i < n; i++)
+            {
+                a++;
+                //= a[i-1]+1;
+                Console.Write($"{a} ");
+            }
+        }
+
+        public class NumericalSeries
+        {
+            private int n;
+            private i
+        }
+    }
+}
