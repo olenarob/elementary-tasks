759a59e26141e4f9524722c8352d318b473d4d96
olenarob <olenarobotko@gmail.com>
2021-05-09 00:08:06 +0300

    Added yield return in Sequence

--- NumericalSequence/Program.cs
+++ NumericalSequence/Program.cs
@@ -9,18 +9,20 @@ namespace NumericalSequence
         static void Main(string[] args)
         {
 
-            if (Argument.TryParse<int>(args[0], 0, int.MaxValue, out int n) &&
-                Argument.TryParse<int>(args[1], 0, int.MaxValue, out int m))
+            if (Argument.TryParse<uint>(args[0], 1, uint.MaxValue, out uint n) &&
+                Argument.TryParse<uint>(args[1], 0, uint.MaxValue, out uint m))
             {
-                int minSqr = (int)Math.Ceiling(Math.Sqrt(m));
+                uint minSqr = (uint)Math.Ceiling(Math.Sqrt(m));
 
                 var naturalNumbers = new NaturalNumbers();
-                naturalNumbers.DisplaySequenceOfLength(n, minSqr);
+                NaturalNumbers.DisplaySeries(naturalNumbers, n, minSqr);
+                var naturalNumbers1 = new NaturalNumbers();
+                NaturalNumbers.DisplaySeries(naturalNumbers1, 0, minSqr, m);
 
-                var fibonacci = new Fibonaccy();
-                fibonacci.DisplaySequenceOfLength(n, 0);
-                if (n < m)
-                fibonacci.DisplaySequenceInRange(n, m);
+                var fibonacci = new Fibonacci();
+                Fibonacci.DisplaySeries(fibonacci, n);
+                var fibonacci1 = new Fibonacci();
+                Fibonacci.DisplaySeries(fibonacci1, 0, minSqr, m);
             }
         }
     }
--- Sequence/Sequence.cs
+++ Sequence/Sequence.cs
@@ -1,60 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sequence
 {
     public abstract class Sequence
     {
-        public abstract int CommonElement(int n);
-
-        public void DisplaySequenceOfLength(int length, int lowerRange)
+        public abstract uint GetNext();
+                    
+        public static IEnumerable<uint> GetSeries(Sequence s, uint length = 0,
+                            uint lowerRange = 0, uint upperRange = uint.MaxValue)
         {
-            int firstIndex = 0;
-            for (int i = 0; i < firstIndex + length; i++)
+            uint n = length;
+            uint j = 0;
+            uint tmp;
+            for (uint i = 0; i <= n + j; i++)
             {
-                if (CommonElement(i) < lowerRange)
+                tmp = s.GetNext();
+                if (tmp < lowerRange)
                 {
-                    firstIndex++;
+                    j++;
                 }
                 else
                 {
-                    Console.Write($"{CommonElement(i)} ");
+                    if (length == 0)
+                    {
+                        if (tmp <= upperRange)
+                        {
+                            n++;
+                        }
+                    }
+                    yield return tmp;
                 }
             }
-            Console.WriteLine();
         }
-        public void DisplaySequenceInRange(int lowerRange, int upperRange)
+        public static void DisplaySeries(Sequence s, uint length = 0,
+                            uint lowerRange = 0, uint upperRange = uint.MaxValue)
         {
-            for (int i = 0; CommonElement(i) <= upperRange; i++)
+            foreach (int number in GetSeries(s, length, lowerRange, upperRange))
             {
-                if (CommonElement(i) >= lowerRange)
-                {
-                    Console.Write($"{CommonElement(i)} ");
-                }
+                Console.Write($"{number} ");
             }
             Console.WriteLine();
         }
     }
     public class NaturalNumbers : Sequence
     {
-        public override int CommonElement(int n)
+        private uint _a = 0;
+        public override uint GetNext()
         {
-            return n switch
-            {
-                0 => 0,
-                _ => (CommonElement(n - 1) + 1),
-            };
+            return _a++;
         }
     }
-    public class Fibonaccy : Sequence
+    
+    public class Fibonacci : Sequence
     {
-        public override int CommonElement(int n)
+        private uint _a = 0;
+        private uint _b = 0;
+        public override uint GetNext()
         {
-            return n switch
+            uint c = _a + _b;
+            if (c == 0)
             {
-                0 => 0,
-                1 => 1,
-                _ => (CommonElement(n - 1) + CommonElement(n - 2)),
-            };
+                _a = 1;
+            }
+            else
+            {
+                _a = _b;
+                _b = c;
+            }
+            return c;
         }
     }
 }
