47841a8853cac61f945ccdf773eac9b82677695f
Olena Robotko <orobo@softserveinc.com>
2022-01-04 19:15:02 +0200

    AnalyzeEnvelopes:
    Made IsInsertedInto() as extension method

--- AnalyzeEnvelopes/EnvelopeAnalyser.cs
+++ AnalyzeEnvelopes/EnvelopeAnalyser.cs
@@ -17,18 +17,18 @@ namespace AnalysisOfEnvelopes
             this.Envelope1 = new Envelope(sides[0], sides[1]);
             this.Envelope2 = new Envelope(sides[2], sides[3]);
 
-            OnNewEnvelope();
+            OnNewPairOfEnvelopes();
         }
 
         public int CheckInsertion()
         {
             int result = 0;
 
-            if (IsInsertedInto(Envelope1, Envelope2))
+            if (Envelope1.IsInsertedInto(Envelope2))
             {
                 result = -1;
             }
-            else if (IsInsertedInto(Envelope2, Envelope1))
+            else if (Envelope2.IsInsertedInto(Envelope1))
             {
                 result = 1;
             }
@@ -36,18 +36,9 @@ namespace AnalysisOfEnvelopes
             return result;
         }
 
-        protected void OnNewEnvelope()
+        protected void OnNewPairOfEnvelopes()
         {
-            Action temp = NewPairOfEnvelopes;
-            if (temp != null)
-            {
-                temp();
-            }
-        }
-
-        private static bool IsInsertedInto(Envelope envelope1, Envelope envelope2)
-        {
-            return (envelope1.ShortSide < envelope2.ShortSide) && (envelope1.LongSide < envelope2.LongSide); ;
+            NewPairOfEnvelopes?.Invoke();
         }
     }
 }
---
+++ AnalyzeEnvelopes/EnvelopeExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace AnalysisOfEnvelopes
+{
+    public static class EnvelopeExtension
+    {
+        public static bool IsInsertedInto(this Envelope envelope1, Envelope envelope2)
+        {
+            if (envelope1 is null || envelope2 is null)
+            {
+                return false;
+            }
+            
+            return (envelope1.ShortSide < envelope2.ShortSide)
+                && (envelope1.LongSide < envelope2.LongSide);
+        }
+    }
+}
